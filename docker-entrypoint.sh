75160b839fe4d0e089a452c05443f0796f9a0a34
Angel Ordaya <xdangel755@gmail.com>
2026-06-14 20:07:39 -0500

    Force Laravel config and cache clearing on startup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+# Clear configuration cache first to prevent old env variables from being used
+php artisan config:clear
+php artisan cache:clear
+php artisan route:clear
+php artisan view:clear
+
 # Run migrations
 php artisan migrate --force
 
-# Cache configuration
+# Re-create cache for production performance
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
